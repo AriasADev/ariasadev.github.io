d5e465f9c56794c4800ae708942847e5703a087e
Aria <aria@shrt.wtf>
2024-05-22 14:30:16 +0100

    Update index.js

--- script/index.js
+++ script/index.js
@@ -20,14 +20,14 @@
 //   color: 0xff3fe8
 // })
 
-VANTA.WAVES({
+VANTA.FOG({
   el: "#background",
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
   minHeight: 200.00,
   minWidth: 200.00,
-  scale: 1.00,
-  scaleMobile: 1.00,
-  zoom: 0.65
+  highlightColor: 0xfd00ff,
+  midtoneColor: 0x9300ff,
+  baseColor: 0xffffff
 })
