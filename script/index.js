a3045edce323c27a9a1692eb4d3fd09953231ba2
Aria <aria@shrt.wtf>
2024-05-22 13:33:43 +0100

    Changed adaptive background styling

--- script/index.js
+++ script/index.js
@@ -1,9 +1,21 @@
 
-VANTA.HALO({
+// VANTA.HALO({
+//  el: "#background",
+//  mouseControls: true,
+//  touchControls: true,
+//  gyroControls: false,
+//  minHeight: 200.00,
+//  minWidth: 200.00
+// })
+
+VANTA.NET({
   el: "#background",
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
   minHeight: 200.00,
-  minWidth: 200.00
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xff3fe8
 })
