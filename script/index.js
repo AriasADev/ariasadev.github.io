d5ec1b5f22f76f57e32b06ab8d294408aaf70a86
Aria <aria@shrt.wtf>
2024-05-22 12:18:37 +0100

    testing animated backghround, and centered content.

---
+++ script/index.js
@@ -0,0 +1,12 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.halo.min.js"></script>
+<script>
+VANTA.HALO({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00
+})
+</script>
