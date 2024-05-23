5456f8df75d70e3ab4d770e7e9332afef8c95a1f
Aria <aria@shrt.wtf>
2024-05-23 12:07:44 +0100

    Changed from fog to halo effect

--- script/index.js
+++ script/index.js
@@ -1,12 +1,12 @@
 
-// VANTA.HALO({
-//  el: "#background",
-//  mouseControls: true,
-//  touchControls: true,
-//  gyroControls: false,
-//  minHeight: 200.00,
-//  minWidth: 200.00
-// })
+VANTA.HALO({
+ el: "#background",
+ mouseControls: true,
+ touchControls: true,
+ gyroControls: false,
+ minHeight: 200.00,
+ minWidth: 200.00
+})
 
 // VANTA.NET({
 //   el: "#background",
@@ -20,14 +20,14 @@
 //   color: 0xff3fe8
 // })
 
-VANTA.FOG({
-  el: "#background",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  highlightColor: 0xfd00ff,
-  midtoneColor: 0x9300ff,
-  baseColor: 0xffffff
-})
+// VANTA.FOG({
+//   el: "#background",
+//   mouseControls: true,
+//   touchControls: true,
+//   gyroControls: false,
+//   minHeight: 200.00,
+//   minWidth: 200.00,
+//   highlightColor: 0xfd00ff,
+//   midtoneColor: 0x9300ff,
+//   baseColor: 0xffffff
+// })
