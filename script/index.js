06f366940d8bc577c373daed3f22495f2d6ac9aa
AriasADev <aria@shrt.wtf>
2024-06-06 12:38:49 +0100

    added animated profile gif, changed styling, removed unnecessary code

--- script/index.js
+++ script/index.js
@@ -1,25 +1,3 @@
-
-// VANTA.HALO({
-//  el: "#background",
-//  mouseControls: true,
-//  touchControls: true,
-//  gyroControls: false,
-//  minHeight: 200.00,
-//  minWidth: 200.00
-// })
-
-// VANTA.NET({
-//   el: "#background",
-//   mouseControls: true,
-//   touchControls: true,
-//   gyroControls: false,
-//   minHeight: 200.00,
-//   minWidth: 200.00,
-//   scale: 1.00,
-//   scaleMobile: 1.00,
-//   color: 0xff3fe8
-// })
-
 VANTA.FOG({
   el: "#background",
   mouseControls: true,
