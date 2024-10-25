be2afb411655690629bb94d437c631a908e5e935
Aria <hellO@ariacodes.com>
2024-10-26 00:09:30 +0100

    Added detection for dark/light mode changing the background styling with it

--- script/index.js
+++ script/index.js
@@ -1,11 +1,21 @@
+function isDarkMode() {
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+
+
+const baseColor = isDarkMode() ? 0x000000 : 0xffffff; // black for dark mode, white for light mode
+const highlightColor = isDarkMode() ? 0xc200b7 : 0xfd00ff; // darker purple for dark mode
+const midtoneColor = isDarkMode() ? 0x7a00cc : 0x9300ff; // darker midtone for dark mode
+
+
 VANTA.FOG({
-  el: "#background",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  highlightColor: 0xfd00ff,
-  midtoneColor: 0x9300ff,
-  baseColor: 0xffffff
-})
+    el: "#background",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    highlightColor: highlightColor,
+    midtoneColor: midtoneColor,
+    baseColor: baseColor
+});
